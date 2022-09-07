42bc221f6484ac86cfc7509c102ace10ecacc10f
Kevin <kevin@rocher-dev.com>
2022-09-07 12:34:32 +0200

    v 0.6.1
    Edit metas :
    - Dynamically update avatar.

--- wpu_extranet.php
+++ wpu_extranet.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: WPU Extranet
 Description: Simple toolbox to create an extranet or a customer account
-Version: 0.6.0
+Version: 0.6.1
 Author: Darklg
 Author URI: https://darklg.me/
 License: MIT License
